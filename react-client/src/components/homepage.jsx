9be55d785d30d2fbaece52cc701f09522b50e228
HdrtPzzzq <45320224+HdrtPzzzq@users.noreply.github.com>
2020-03-10 03:27:22 +0100

    css updated

--- react-client/src/components/homepage.jsx
+++ react-client/src/components/homepage.jsx
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 class HomePage extends Component {
   render() {
     return (
-      <div>
-        Home page
+      <div class="pseudo">
+        HdrtPzzzq
+        <div class="avatar"></div>
       </div>
     )
   }
