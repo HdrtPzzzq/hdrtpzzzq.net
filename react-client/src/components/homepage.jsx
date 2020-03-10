dd58a91cd9c75e4db91173fbccbef46ccd83136a
Loic Segundo <loic.segundo@epita.fr>
2020-03-10 13:13:30 +0000

    resolved issue with require(image)

--- react-client/src/components/homepage.jsx
+++ react-client/src/components/homepage.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import avatar from '../images/mrpokeylope.png'
+console.log(avatar);
 class HomePage extends Component {
   render() {
     return (
-      <div class="pseudo">
+      <div>
+        <div className="pseudo">
         HdrtPzzzq
-        <div class="avatar"></div>
+        </div>
+        <div className="avatar">
+          <img src={avatar} alt="hdrtpzzzq_avatar"/>
+        </div>
       </div>
     )
   }
